9f69497a06d00779ca85879ff1ae85e66464462d
RD211 <rdyt211@gmail.com>
2020-07-08 16:35:01 +0300

    Removed useless stuff

---
+++ HumanBenchmark/Main.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace HumanBenchmark
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_reactionGame = new System.Windows.Forms.Button();
+            this.btn_targetBot = new System.Windows.Forms.Button();
+            this.reactionBotTimer = new System.Windows.Forms.Timer(this.components);
+            this.targetBotTimer = new System.Windows.Forms.Timer(this.components);
+            this.btn_type = new System.Windows.Forms.Button();
+            this.updateMouseLabel = new System.Windows.Forms.Timer(this.components);
+            this.txt_read = new System.Windows.Forms.RichTextBox();
+            this.btn_read = new System.Windows.Forms.Button();
+            this.btn_visualMemory = new System.Windows.Forms.Button();
+            this.visualMemoryTimer = new System.Windows.Forms.Timer(this.components);
+            this.safetyTimer = new System.Windows.Forms.Timer(this.components);
+            this.btn_verbalMemory = new System.Windows.Forms.Button();
+            this.verbalMemoryTimer = new System.Windows.Forms.Timer(this.components);
+            this.btn_numberMemory = new System.Windows.Forms.Button();
+            this.numberMemoryTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btn_reactionGame
+            // 
+            this.btn_reactionGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.btn_reactionGame.Location = new System.Drawing.Point(13, 4);
+            this.btn_reactionGame.Name = "btn_reactionGame";
+            this.btn_reactionGame.Size = new System.Drawing.Size(185, 121);
+            this.btn_reactionGame.TabIndex = 0;
+            this.btn_reactionGame.Text = "Start reaction bot";
+            this.btn_reactionGame.UseVisualStyleBackColor = true;
+            this.btn_reactionGame.Click += new System.EventHandler(this.btn_reactionGame_Click);
+            // 
+            // btn_targetBot
+            // 
+            this.btn_targetBot.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.btn_targetBot.Location = new System.Drawing.Point(204, 4);
+            this.btn_targetBot.Name = "btn_targetBot";
+            this.btn_targetBot.Size = new System.Drawing.Size(185, 121);
+            this.btn_targetBot.TabIndex = 1;
+            this.btn_targetBot.Text = "Start target bot";
+            this.btn_targetBot.UseVisualStyleBackColor = true;
+            this.btn_targetBot.Click += new System.EventHandler(this.btn_targetBot_Click);
+            // 
+            // reactionBotTimer
+            // 
+            this.reactionBotTimer.Interval = 1;
+            this.reactionBotTimer.Tick += new System.EventHandler(this.reactionBotTimer_Tick);
+            // 
+            // targetBotTimer
+            // 
+            this.targetBotTimer.Interval = 5;
+            this.targetBotTimer.Tick += new System.EventHandler(this.targetBotTimer_Tick);
+            // 
+            // btn_type
+            // 
+            this.btn_type.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.btn_type.Location = new System.Drawing.Point(395, 4);
+            this.btn_type.Name = "btn_type";
+            this.btn_type.Size = new System.Drawing.Size(185, 121);
+            this.btn_type.TabIndex = 3;
+            this.btn_type.Text = "Start typing";
+            this.btn_type.UseVisualStyleBackColor = true;
+            this.btn_type.Click += new System.EventHandler(this.btn_chimp_Click);
+            // 
+            // txt_read
+            // 
+            this.txt_read.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.txt_read.Location = new System.Drawing.Point(13, 132);
+            this.txt_read.Name = "txt_read";
+            this.txt_read.Size = new System.Drawing.Size(567, 248);
+            this.txt_read.TabIndex = 5;
+            this.txt_read.Text = "";
+            // 
+            // btn_read
+            // 
+            this.btn_read.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.btn_read.Location = new System.Drawing.Point(468, 334);
+            this.btn_read.Name = "btn_read";
+            this.btn_read.Size = new System.Drawing.Size(112, 46);
+            this.btn_read.TabIndex = 6;
+            this.btn_read.Text = "Read";
+            this.btn_read.UseVisualStyleBackColor = true;
+            this.btn_read.Click += new System.EventHandler(this.btn_readChimp_Click);
+            // 
+            // btn_visualMemory
+            // 
+            this.btn_visualMemory.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.btn_visualMemory.Location = new System.Drawing.Point(586, 4);
+            this.btn_visualMemory.Name = "btn_visualMemory";
+            this.btn_visualMemory.Size = new System.Drawing.Size(185, 121);
+            this.btn_visualMemory.TabIndex = 7;
+            this.btn_visualMemory.Text = "Start visual memory";
+            this.btn_visualMemory.UseVisualStyleBackColor = true;
+            this.btn_visualMemory.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // visualMemoryTimer
+            // 
+            this.visualMemoryTimer.Interval = 20;
+            this.visualMemoryTimer.Tick += new System.EventHandler(this.visualMemoryTimer_Tick);
+            // 
+            // safetyTimer
+            // 
+            this.safetyTimer.Enabled = true;
+            this.safetyTimer.Interval = 250;
+            this.safetyTimer.Tick += new System.EventHandler(this.safetyTimer_Tick);
+            // 
+            // btn_verbalMemory
+            // 
+            this.btn_verbalMemory.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.btn_verbalMemory.Location = new System.Drawing.Point(586, 132);
+            this.btn_verbalMemory.Name = "btn_verbalMemory";
+            this.btn_verbalMemory.Size = new System.Drawing.Size(185, 121);
+            this.btn_verbalMemory.TabIndex = 8;
+            this.btn_verbalMemory.Text = "Start verbal memory";
+            this.btn_verbalMemory.UseVisualStyleBackColor = true;
+            this.btn_verbalMemory.Click += new System.EventHandler(this.btn_verbalMemory_Click);
+            // 
+            // verbalMemoryTimer
+            // 
+            this.verbalMemoryTimer.Interval = 400;
+            this.verbalMemoryTimer.Tick += new System.EventHandler(this.verbalMemoryTimer_Tick);
+            // 
+            // btn_numberMemory
+            // 
+            this.btn_numberMemory.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.btn_numberMemory.Location = new System.Drawing.Point(586, 259);
+            this.btn_numberMemory.Name = "btn_numberMemory";
+            this.btn_numberMemory.Size = new System.Drawing.Size(185, 121);
+            this.btn_numberMemory.TabIndex = 9;
+            this.btn_numberMemory.Text = "Start number memory";
+            this.btn_numberMemory.UseVisualStyleBackColor = true;
+            this.btn_numberMemory.Click += new System.EventHandler(this.btn_numberMemory_Click);
+            // 
+            // numberMemoryTimer
+            // 
+            this.numberMemoryTimer.Interval = 50;
+            this.numberMemoryTimer.Tick += new System.EventHandler(this.numberMemoryTimer_Tick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 389);
+            this.Controls.Add(this.btn_numberMemory);
+            this.Controls.Add(this.btn_verbalMemory);
+            this.Controls.Add(this.btn_visualMemory);
+            this.Controls.Add(this.btn_read);
+            this.Controls.Add(this.txt_read);
+            this.Controls.Add(this.btn_type);
+            this.Controls.Add(this.btn_targetBot);
+            this.Controls.Add(this.btn_reactionGame);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MinimumSize = new System.Drawing.Size(793, 428);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dashboard";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_reactionGame;
+        private System.Windows.Forms.Button btn_targetBot;
+        private System.Windows.Forms.Timer reactionBotTimer;
+        private System.Windows.Forms.Timer targetBotTimer;
+        private System.Windows.Forms.Button btn_type;
+        private System.Windows.Forms.Timer updateMouseLabel;
+        private System.Windows.Forms.RichTextBox txt_read;
+        private System.Windows.Forms.Button btn_read;
+        private System.Windows.Forms.Button btn_visualMemory;
+        private System.Windows.Forms.Timer visualMemoryTimer;
+        private System.Windows.Forms.Timer safetyTimer;
+        private System.Windows.Forms.Button btn_verbalMemory;
+        private System.Windows.Forms.Timer verbalMemoryTimer;
+        private System.Windows.Forms.Button btn_numberMemory;
+        private System.Windows.Forms.Timer numberMemoryTimer;
+    }
+}
+
---
+++ HumanBenchmark/Main.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Patagames.Ocr;
+using Patagames.Ocr.Enums;
+using System.Windows.Input;
+namespace HumanBenchmark
+{
+    public partial class Main : Form
+    {
+        #region Constructor and on load
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region Reaction time game
+        int reactedThisManyTimes = 0;
+        List<Color> colors = new List<Color>() { Color.FromArgb(43, 135, 209), Color.FromArgb(206, 38, 54), Color.FromArgb(75, 219, 106) };
+        private void btn_reactionGame_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.Sleep(2000);
+            reactionBotTimer.Enabled = true;
+            reactionBotTimer.Start();
+            
+        }
+
+        private void reactionBotTimer_Tick(object sender, EventArgs e)
+        {
+            var pixel = ScreenCapture.GetColorAtTwo(300,300);
+            for (int i = 0; i < 3; i++)
+            {
+                if (Math.Abs(colors[i].R - pixel.R) + Math.Abs(colors[i].B - pixel.B) + Math.Abs(colors[i].G - pixel.G) <= 10)
+                {
+                    if (i != 1)
+                    {
+                        MouseOperations.SetCursorPosition(300, 300);
+                        MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftDown);
+                        MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftUp);
+                        reactedThisManyTimes++;
+                        System.Threading.Thread.Sleep(400);
+                    }
+                }
+            }
+            if (reactedThisManyTimes == 10)
+            {
+                reactionBotTimer.Enabled = false;
+                reactionBotTimer.Stop();
+                reactedThisManyTimes = 0;
+            }
+        }
+        #endregion
+
+        #region Target game
+        int shotThisManyTimes = 0;
+        private void btn_targetBot_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.Sleep(2000);
+            targetBotTimer.Start();
+            targetBotTimer.Enabled = true;
+        }
+
+        private void targetBotTimer_Tick(object sender, EventArgs e)
+        {
+            if(shotThisManyTimes==150)
+            {
+                targetBotTimer.Stop();
+                targetBotTimer.Enabled = false;
+                shotThisManyTimes = 0;
+            }
+            var bmp = ScreenCapture.CaptureActiveWindow();
+            for (int i = 0; i < 400; i += 2)
+            {
+                for (int j = 0; j < 1400; j += 2)
+                {
+                    var pixel = bmp.GetPixel(j + 300, i + 300);
+                    if (Math.Abs(255 - pixel.R) + Math.Abs(255 - pixel.G) + Math.Abs(255 - pixel.B) <= 10)
+                    {
+
+                        MouseOperations.SetCursorPosition(j + 300,i+300);
+                        MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftDown);
+                        MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftUp);
+                        shotThisManyTimes++;
+                        return;
+                    }
+
+                }
+            }
+
+        }
+        #endregion
+
+        #region Typing game
+        private static Bitmap cropImage(DirectBitmap img, Rectangle cropArea)
+        {
+            return img.Bitmap.Clone(cropArea, img.Bitmap.PixelFormat);
+        }
+        private void btn_chimp_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.Sleep(2000);
+            SendKeys.Send(this.txt_read.Text);
+        }
+        private void btn_readChimp_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.Sleep(2000);
+            var bmp = ScreenCapture.CaptureActiveWindow();
+            using (var api = OcrApi.Create())
+            {
+                api.Init(Languages.English);
+                string plainText = api.GetTextFromImage(cropImage(bmp, new Rectangle(473, 364, 1419 - 473, 520 - 364)));
+                bmp.Dispose();
+                if (plainText[0] == '[')
+                    plainText = plainText.Remove(0, 1);
+                this.txt_read.Text = plainText.Replace("\n", "").Replace("\r", "");
+            }
+        }
+        #endregion
+
+        #region Visual memory game
+        //560 200
+        //1400 630
+        int levelsPassed = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.Sleep(2000);
+            visualMemoryTimer.Enabled = true;
+            visualMemoryTimer.Start();
+        }
+
+        private void visualMemoryTimer_Tick(object sender, EventArgs e)
+        {
+            if(levelsPassed==50)
+            {
+                levelsPassed = 0;
+                visualMemoryTimer.Stop();
+                visualMemoryTimer.Enabled = false;
+            }
+            levelsPassed++;
+            var bmp = ScreenCapture.CaptureActiveWindow();
+
+            bmp = new DirectBitmap(cropImage(bmp, new Rectangle(560, 200, 1400 - 560, 430)));
+            List<Point> pointsToPress = new List<Point>();
+            for(int i = 0;i<bmp.Height;i+=20)
+            {
+                for(int j = 0;j<bmp.Width;j+=20)
+                {
+                    int r = 0, g = 0, b = 0;
+                    for(int ii = 0;ii<5;ii++)
+                    {
+                        for(int jj=0;jj<5;jj++)
+                        {
+                            var pixel = bmp.GetPixel(j + jj, i + ii);
+                            r += pixel.R;
+                            b += pixel.B;
+                            g += pixel.G;
+                        }
+                    }
+                    if(r+g+b>=250*3*25)
+                    {
+                        pointsToPress.Add(new Point(j, i));
+                    }
+                }
+            }
+            bmp.Dispose();
+            System.Threading.Thread.Sleep(500);
+            bool same = true;
+            while(same)
+            {
+                same = false;
+                var newBmp = ScreenCapture.CaptureActiveWindow();
+
+                newBmp = new DirectBitmap(cropImage(newBmp, new Rectangle(560, 200, 1400 - 560, 430)));
+                for (int i = 0; i < newBmp.Height; i += 20)
+                {
+                    for (int j = 0; j < newBmp.Width; j += 20)
+                    {
+                        int r = 0, g = 0, b = 0;
+                        for (int ii = 0; ii < 5; ii++)
+                        {
+                            for (int jj = 0; jj < 5; jj++)
+                            {
+                                var pixel = newBmp.GetPixel(j + jj, i + ii);
+                                r += pixel.R;
+                                b += pixel.B;
+                                g += pixel.G;
+                            }
+                        }
+                        if (r + g + b >= 250 * 3 * 25)
+                        {
+                            same = true;
+                        }
+                    }
+                }
+                newBmp.Dispose();
+                System.Threading.Thread.Sleep(500);
+            }
+            pointsToPress.ForEach((point) => {
+                MouseOperations.SetCursorPosition(point.X + 560, point.Y + 200);
+                MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftDown);
+                MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftUp);
+
+            });
+            GC.Collect();
+            System.Threading.Thread.Sleep(750);
+        }
+        #endregion
+
+        #region Verbal memory game
+        int words = 0;
+        Dictionary<string, bool> wordsSoFar = new Dictionary<string, bool>();
+        private void btn_verbalMemory_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.Sleep(2000);
+            wordsSoFar = new Dictionary<string, bool>();
+            verbalMemoryTimer.Enabled = true;
+            verbalMemoryTimer.Start();
+        }
+
+        private void verbalMemoryTimer_Tick(object sender, EventArgs e)
+        {
+            if(words>=200000)
+            {
+                words = 0;
+                verbalMemoryTimer.Stop();
+            }
+            words++;
+            var bmp = ScreenCapture.CaptureActiveWindow();
+            using (var api = OcrApi.Create())
+            {
+                api.Init(Languages.English);
+                var croppedImage = cropImage(bmp, new Rectangle(550, 340, 1350 - 550, 437 - 340));
+                Bitmap resized = new Bitmap(croppedImage, new Size(croppedImage.Width / 2, croppedImage.Height / 2));
+                string plainText = api.GetTextFromImage(resized);
+                resized.Dispose();
+                croppedImage.Dispose();
+
+                if (wordsSoFar.ContainsKey(plainText))
+                {
+                    MouseOperations.SetCursorPosition(868, 480);
+                    MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftDown);
+                    MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftUp);
+                }
+                else
+                {
+                    wordsSoFar.Add(plainText, true);
+                    //1040 480
+                    MouseOperations.SetCursorPosition(1040, 480);
+                    MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftDown);
+                    MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftUp);
+                }
+            }
+
+
+            bmp.Dispose();
+            GC.Collect();
+
+        }
+        #endregion
+
+        #region Number memory game
+        int numbers = 0;
+        private void btn_numberMemory_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.Sleep(2000);
+            numberMemoryTimer.Enabled = true;
+            numberMemoryTimer.Start();
+        }
+        private void numberMemoryTimer_Tick(object sender, EventArgs e)
+        {
+            if(numbers==100)
+            {
+                numbers = 0;
+                numberMemoryTimer.Stop();
+                numberMemoryTimer.Enabled = false;
+            }
+            numbers++;
+            var bmp = ScreenCapture.CaptureActiveWindow();
+            using (var api = OcrApi.Create())
+            {
+                api.Init(Languages.English);
+                int other = 0;
+                if(numbers>=18)
+                {
+                    other = 200;
+                }
+                var croppedImage = cropImage(bmp, new Rectangle(350, 150, 1650 - 350, 425 - 150+other));
+                Bitmap resized = new Bitmap(croppedImage, new Size(croppedImage.Width / 4, croppedImage.Height / 4));
+                croppedImage.Dispose();
+
+                string plainText = api.GetTextFromImage(resized);
+                resized.Dispose();
+                plainText = plainText.Replace("O", "0");
+                plainText = plainText.Replace("o", "0");
+                plainText = plainText.Replace(" ", "");
+                plainText = plainText.Replace("_", "");
+                plainText = plainText.Replace("-", "");
+                bool wait = true;
+                while(wait)
+                {
+                    var newBmp = ScreenCapture.CaptureActiveWindow();
+                    var newCroppedImage = cropImage(newBmp, new Rectangle(350, 150, 1650 - 350, 425 - 150+other));
+                    Bitmap newResized = new Bitmap(newCroppedImage, new Size(newCroppedImage.Width / 4, newCroppedImage.Height / 4));
+                    newCroppedImage.Dispose();
+
+                    string plainText2 = api.GetTextFromImage(newResized);
+                    newResized.Dispose();
+                    plainText2 = plainText2.Replace("O", "0");
+                    plainText2 = plainText2.Replace("o", "0");
+
+                    plainText2 = plainText2.Replace(" ", "");
+                    plainText2 = plainText2.Replace("_", "");
+                    plainText2 = plainText2.Replace("-", "");
+
+
+                    if (plainText!=plainText2)
+                    {
+                        wait = false;
+                    }
+                    System.Threading.Thread.Sleep(1000);
+                    newBmp.Dispose();
+                    GC.Collect();
+                }
+                MouseOperations.SetCursorPosition(990, 384);
+                MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftDown);
+                MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftUp);
+                System.Threading.Thread.Sleep(1000);
+                SendKeys.Send(plainText.Replace("\n","").Replace("\r",""));
+                System.Threading.Thread.Sleep(2000);
+                SendKeys.Send("\n");
+
+            }
+            System.Threading.Thread.Sleep(2000);
+            SendKeys.Send("\n");
+            System.Threading.Thread.Sleep(10);
+            MouseOperations.SetCursorPosition(100, 100);
+            //962 542
+            bmp.Dispose();
+            GC.Collect();
+        }
+        #endregion
+
+        #region Safety
+        private void safetyTimer_Tick(object sender, EventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.Escape))
+            {
+                Environment.Exit(0);
+            }
+        }
+        #endregion
+
+    }
+}
